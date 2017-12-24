7ebfd0cf565c0765b0e287266fd0e89ac89f75c4
Syed Faizan <smfai200@gmail.com>
2017-12-24 19:58:34 +0500

    Update and rename 7. Tree to 7. Binary-Tree/Node.java

---
+++ 7. Binary-Tree/Node.java
@@ -0,0 +1,35 @@
+package tree;
+
+import java.util.Scanner;
+
+public class Node {
+    Node left,right,temp,cur,pre = null;
+    int data;
+    int flag;
+    
+    public void insert(Node root, int value){
+        if(root == null){ //ROOT Node to be Created
+            cur = new Node();
+            cur.left = null;
+            cur.right = null;
+            cur.data = value;
+            if(flag == 0)
+                pre.right = cur;
+            else
+                pre.left = cur;
+        }else{
+            if(value > root.data){
+                flag = 0;
+                pre = root;
+                insert(root.right,value);
+            }else if(value < root.data){
+                flag = 1;
+                pre = root;
+                insert(root.left,value);
+            }else{
+                System.out.println("No Duplication Allowed");
+            }
+        }
+    }  
+}
+    
